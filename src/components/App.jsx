35952f379a8f95619d32f4ab30de62f40496cda6
khrystyna-shekel <christina.shekel@gmail.com>
2023-12-09 22:20:40 +0200

    gallery-fetched

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    ></div>
-  );
-};
+import React, { Component } from 'react';
+import { SearchBar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Modal } from './Modal/Modal';
+import { Loader } from './Loader/Loader';
+import { fetchGallary } from 'services/api';
+
+export class App extends Component {
+  state = {
+    searchQuery: '',
+    searchResult: [],
+    page: 1,
+    isLoading: false,
+    isModalOpen: false,
+  };
+
+  async componentDidMount() {
+    try {
+      this.setState({ isLoading: true });
+
+      const allGallery = await fetchGallary();
+      this.setState({ searchResult: allGallery.hits });
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+  };
+
+  openModal = e => {
+    console.log('bun');
+  };
+
+  closeModal = e => {};
+
+  handleLoadBtn = e => {
+    console.log('no');
+  };
+
+  render() {
+    const { searchResult, isModalOpen, isLoading } = this.state;
+    return (
+      <div>
+        <SearchBar onSubmit={this.onSubmit} />
+        <ImageGallery images={searchResult} openModal={this.openModal} />
+        <Button handleLoadBtn={this.handleLoadBtn} />
+        {isLoading && <Loader />}
+        {isModalOpen && <Modal closeModal={this.closeModal} />}
+      </div>
+    );
+  }
+}
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
+import styled from 'styled-components';
+
+export const ImageGallery = ({ images }) => {
+  return (
+    <StyledGalleryList>
+      {images.map(({ id, webformatURL, largeImageURL }) => {
+        return (
+          <ImageGalleryItem
+            key={id}
+            webformatURL={webformatURL}
+            largeImageURL={largeImageURL}
+          />
+        );
+      })}
+    </StyledGalleryList>
+  );
+};
+
+const StyledGalleryList = styled.ul`
+  display: grid;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-gap: 16px;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding: 25px 0;
+  list-style: none;
+  margin-left: auto;
+  margin-right: auto;
+`;
